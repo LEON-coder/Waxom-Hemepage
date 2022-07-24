1762c0b2995ffb6dff77079f8113fb4bc9deccd1
Leonid <leonid.spb2015@yandex.ru>
2022-07-24 19:00:18 +0300

    Добавил бургер-меню для мобильной весрии экрана

--- dev/js/burger-menu.js
+++ dev/js/burger-menu.js
@@ -1,7 +1,7 @@
 
 let burger = document.getElementById('burger-menu');
 burger.addEventListener('click', burgerMenu);
-
+burger.addEventListener('touchend', burgerMenu);
 function burgerMenu() {
 var navigation = document.getElementById('class__list-nav');
 navigation.classList.toggle('open');
