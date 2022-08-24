f85db3fe6d7e6189275492252eb62b9164c8cdc4
Leonid <leonid.spb2015@yandex.ru>
2022-08-24 11:08:04 +0300

    Корректировка работы бургер-меню

--- build/burger/burger-menu.js
+++ build/burger/burger-menu.js
@@ -2,13 +2,10 @@
 let burger = document.querySelector('.burger-menu');
 burger.addEventListener('click', burgerMenu);
 let navigation = document.querySelector('.nav__list');
+burger.addEventListener('click', burgerMenuclose);
+
 
 function burgerMenu() {
 navigation.classList.toggle('open');
-burger.classList.toggle("active");
+burger.classList.toggle("active--burger");
 } 
-
-
-
-
-
